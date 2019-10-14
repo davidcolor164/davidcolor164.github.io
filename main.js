0a01e31f13623e99a38566622ee5082096f17574
davidcolor164 <davidcolor164@gmail.com>
2019-10-15 00:15:10 +0700

    navbar complete 90%

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-﻿ $(function(){
- 
-})  
- 
+﻿$(document).ready(function() {
+	$('.navbar .navbar-nav .nav-item').on('click', function() {
+		console.log('sdfsdf');
+		$(this).siblings().removeClass('open');
+		$(this).toggleClass('open');
+	});
+});
