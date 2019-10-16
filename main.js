9fb665a9839ca89ca6bf33c56f306a8034d0cf1b
davidcolor164 <davidcolor164@gmail.com>
2019-10-16 16:17:03 +0700

    completed index

--- main.js
+++ main.js
@@ -4,4 +4,15 @@
 		$(this).siblings().removeClass('open');
 		$(this).toggleClass('open');
 	});
+
+
+	//jquery for tabs-products-home
+	$('.tabs-name li h3').click(function(event) {
+		$('.tabs-name li h3').removeClass('active');
+		$(this).addClass('active');
+
+		chiso = $(this).parents().index() +1;
+		$('ul.tabs-content li .products').removeClass('show-products');
+		$('ul.tabs-content li:nth-child(' + chiso + ") .products").addClass('show-products');
+	});
 });
